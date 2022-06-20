b3a3b9e528deeecd9ac165c36c2781861d59efd0
AlexanderKroll <74175710+AlexanderKroll@users.noreply.github.com>
2022-06-20 10:08:18 +0200

    Adding prediction functions for GEMs

---
+++ code/directory_infomation.py
@@ -0,0 +1,8 @@
+#specify the directory that contains the folder with all datsets:
+datasets_dir = "..\\data\\"
+
+#specifying path with additional code:
+import sys
+sys.path.append('.\\additional_code')
+sys.path.append('.')
+sys.path.append('..\\..\\notebooks_and_code\\additional_code')
